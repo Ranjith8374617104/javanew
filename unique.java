3bcffc79095f22eb3f8a09f158cbdfc1c3af4b52
Ranjith8374617104 <154883026+Ranjith8374617104@users.noreply.github.com>
2025-03-05 15:12:44 +0530

    004

---
+++ unique.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class unique{
+    public static void main(String[] args){
+        int ans=0;
+        for(int i=0;i<n;i++)
+        ans^=arr[i];
+        return ans;
+    }
+}
