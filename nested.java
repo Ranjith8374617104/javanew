d3360cef1d740660888f89c401dea98ca5dc7f9c
Ranjith8374617104 <154883026+Ranjith8374617104@users.noreply.github.com>
2025-03-05 15:19:05 +0530

    09,10,11

--- nested.java
+++ nested.java
@@ -1,25 +1,17 @@
 import java.util.*;
-public class nested{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //local variable for the class and global variable main method
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
-        if(a<b){
-            if(a<c){
-                System.out.println("A is smaller");
-            }else if{
-
-                System.out.println("c is smaller");
-            }else{
-                if(b<a){
-                    System.out.println("B is smaller");
-                }else{
-                    Sytem.out.println("c is smaller");
-                }
-                }
-            }
-            }
+interface out{
+    void outm();
+    interface in{
+        void innm();
+    }
 }
-
+class nested implements out.in{
+    public void innm()
+    {
+        System.out.println("hello i'm inner method");
+    }
+    public static void main(String[] args){
+        nested h=new nested();
+        h.innm();
+    }
+}
--- power.java
+++ power.java
@@ -1,11 +1,11 @@
-import java.util.*;
-public class power{
+class power{
+    static int pow(int b,int ex)
+    {
+        if(ex==0) return 1;
+        return b* pow(b,ex-1);
+    }
     public static void main(String[] args){
-        int a=8388608;
-                if((a&(a-1))==0){
-            System.out.println("power of 2");
-        }else{
-            System.out.println("not a power of 2");
-        }
+        int res=pow(7,8);
+        System.out.println(res);
     }
 }
